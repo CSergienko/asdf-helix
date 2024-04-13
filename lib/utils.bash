133c5e33fe2f7c9baf6233c935dbd2adf9f1dac7
Chris Sergienko <iam@theonewho.codes>
2024-04-14 01:31:58 +0300

    fix release tar extension

--- lib/utils.bash
+++ lib/utils.bash
@@ -42,7 +42,7 @@ download_release() {
 	architecture="x86_64"
 	os="linux"
 
-	url="$GH_REPO/archive/helix-${version}-${architecture}-${os}.tar.gz"
+	url="$GH_REPO/archive/helix-${version}-${architecture}-${os}.tar.xz"
 
 	echo "* Downloading $TOOL_NAME release $version..."
 	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
